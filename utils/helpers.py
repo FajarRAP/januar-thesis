4c3a1317c3c5a7358a80fde95f37b5df26ef1a0b
Fajar Riansyah <fajary781@gmail.com>
2026-06-10 06:44:40 +0700

    feat (helpers): add min_max_normalization function for data normalization

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,2 +1,7 @@
+import pandas as pd
+
 def to_percent(number):
     return f"{number * 100:.2f}%"
+
+def min_max_normalization(dataframe: pd.DataFrame):
+  return (dataframe - dataframe.min()) / (dataframe.max() - dataframe.min())
