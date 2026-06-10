25271e6854502210336ddb7c220ed42dd7863466
Fajar Riansyah <fajary781@gmail.com>
2026-06-10 08:13:42 +0700

    feat: update min_max_normalization function to handle scalar values

--- utils/helpers.py
+++ utils/helpers.py
@@ -3,5 +3,8 @@ import pandas as pd
 def to_percent(number):
     return f"{number * 100:.2f}%"
 
-def min_max_normalization(dataframe: pd.DataFrame):
-  return (dataframe - dataframe.min()) / (dataframe.max() - dataframe.min())
+def min_max_normalization(value, min_value, max_value):
+    if min_value == max_value: 
+        return 0.0
+
+    return (value - min_value) / (max_value - min_value)
